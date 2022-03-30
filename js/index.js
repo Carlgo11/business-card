ef8a60a818454f6d39aa0ba3028a8ea90ccf8ff7
Carlgo11 <carlgo11@users.noreply.github.com>
2022-03-31 00:35:28 +0200

    Clean up code

--- js/index.js
+++ js/index.js
@@ -1,19 +1,12 @@
+let qrcode = new QRCode("qrcode");
 
-$('document').ready(function () {
-    let qrcode = new QRCode("qrcode",
-        {
-            width: 256,
-            height: 256,
-            colorDark: "#000000",
-            colorLight: "#ffffff",
-            correctLevel: QRCode.CorrectLevel.H
-        });
-    $('li').click(function() {
-        $("#socials").hide();
-        $("#qr").show();
-        qrcode.makeCode($(this).data('url'));
-    });
-    $('#profile-pic').click(()  => {
-        window.location.reload();
-    });
+$('li').click(function () {
+    $("#socials").hide();
+    $("#qr").show();
+    qrcode.makeCode($(this).data('url'));
+});
+
+$('#profile-pic').click(() => {
+    $("#socials").show();
+    $("#qr").hide();
 });
