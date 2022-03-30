cea3225e9be0d0bc4ee015c29989e00d046af476
Carlgo11 <carlgo11@users.noreply.github.com>
2022-03-30 14:56:33 +0200

    Add other socials

--- js/index.js
+++ js/index.js
@@ -1,12 +1,19 @@
+
 $('document').ready(function () {
     let qrcode = new QRCode("qrcode",
         {
-            text: `${$('#qrcode').data('user')}`,
             width: 256,
             height: 256,
             colorDark: "#000000",
             colorLight: "#ffffff",
             correctLevel: QRCode.CorrectLevel.H
         });
-    qrcode.makeCode();
+    $('li').click(function() {
+        $("#socials").hide();
+        $("#qr").show();
+        qrcode.makeCode($(this).data('url'));
+    });
+    $('#profile-pic').click(()  => {
+        window.location.reload();
+    });
 });
