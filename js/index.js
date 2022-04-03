fde6f7c9425a18a0cc62a139d9f1e3ba30ac749f
Carlgo11 <carlgo11@users.noreply.github.com>
2022-04-03 12:02:14 +0200

    Add dark theme support

--- js/index.js
+++ js/index.js
@@ -1,4 +1,9 @@
-let qrcode = new QRCode('qrcode', {height: 208, width: 208});
+let options = {height: 208, width: 208}
+if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+    jQuery.extend(options, {colorDark: "#eee", colorLight: "#000"});
+}
+
+let qrcode = new QRCode('qrcode', options);
 
 $('li').click(function () {
     $('#socials').hide();
